881f9c3f22f76661f0dafb93de54604d86dd2928
julianPrieto1323 <9000169@alumnos.ufv.es>
2023-06-08 15:41:31 +0200

    Programada la funcionalidad de añadir compras

---
+++ skeleton-starter-flow-spring-14/skeleton-starter-flow-spring-14/src/main/java/com/PlantillaFront/Anhadir_Compras.java
@@ -0,0 +1,29 @@
+package com.PlantillaFront;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.textfield.TextField;
+import org.springframework.util.StringValueResolver;
+import org.vaadin.example.Compras;
+import org.vaadin.example.Persona;
+
+import java.net.URISyntaxException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.Locale;
+
+public class Anhadir_Compras extends VerticalLayout {
+    DataService data = new DataService();
+    public void Anhadir_ComprasView(Persona persona){
+        
+
+
+    }
+
+}
