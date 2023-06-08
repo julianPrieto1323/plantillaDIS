266a12a580cb25ec25f93de93c6d10534983e5be
julianPrieto1323 <9000169@alumnos.ufv.es>
2023-06-08 15:13:57 +0200

    Creada la vista de registro

--- skeleton-starter-flow-spring-14/skeleton-starter-flow-spring-14/src/main/java/com/PlantillaFront/RegisterView.java
+++ skeleton-starter-flow-spring-14/skeleton-starter-flow-spring-14/src/main/java/com/PlantillaFront/RegisterView.java
@@ -15,6 +15,46 @@ import java.util.ArrayList;
 
 public class RegisterView extends VerticalLayout {
     public void paginaRegister() {
-        
+        TextField Nombre = new TextField("First name");
+        PasswordField password = new PasswordField("Password");
+
+        PasswordField confirmPassword = new PasswordField("Confirm password");
+
+        Button registrate = new Button("Registrate");
+        Button atras = new Button("Atrás");
+        atras.addThemeVariants(ButtonVariant.LUMO_PRIMARY, ButtonVariant.LUMO_ERROR);
+        HorizontalLayout h1 = new HorizontalLayout();
+        HorizontalLayout h2 = new HorizontalLayout();
+        HorizontalLayout h3 = new HorizontalLayout();
+
+        h1.add(Nombre);
+        h2.add(password, confirmPassword);
+        h3.add(registrate, atras);
+        this.add(h1, h2, h3);
+
+        registrate.addClickListener(buttonClickEvent -> {
+            DataService dataService = new DataService();
+            ArrayList<Persona> listadoUsuarios = new ArrayList<Persona>();
+            try {
+                listadoUsuarios = dataService.mostrarUsers();
+            } catch (URISyntaxException e) {
+                throw new RuntimeException(e);
+            }
+            Persona persona = new Persona();
+            persona.setNombre(Nombre.getValue());
+            persona.setPassword(password.getValue());
+            dataService.RegistrarPersona(persona, listadoUsuarios);
+            removeAll();
+            LoginView loginView = new LoginView();
+            loginView.PaginaLoginWiew();
+            this.add(loginView);
+        });
+
+        atras.addClickListener(buttonClickEvent -> {
+            removeAll();
+            LoginView loginView = new LoginView();
+            loginView.PaginaLoginWiew();
+            this.add(loginView);
+        });
     }
 }
