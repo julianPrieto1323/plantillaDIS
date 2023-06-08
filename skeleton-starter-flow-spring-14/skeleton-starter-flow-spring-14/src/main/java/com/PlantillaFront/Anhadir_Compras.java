80b8f5a428853fd647541b0304e9802c335e1f12
julianPrieto1323 <9000169@alumnos.ufv.es>
2023-06-08 15:42:19 +0200

    Programada la funcionalidad de editar usuario

--- skeleton-starter-flow-spring-14/skeleton-starter-flow-spring-14/src/main/java/com/PlantillaFront/Anhadir_Compras.java
+++ skeleton-starter-flow-spring-14/skeleton-starter-flow-spring-14/src/main/java/com/PlantillaFront/Anhadir_Compras.java
@@ -21,7 +21,53 @@ import java.util.Locale;
 public class Anhadir_Compras extends VerticalLayout {
     DataService data = new DataService();
     public void Anhadir_ComprasView(Persona persona){
-        
+        Compras compras = new Compras();
+
+        H1 h1 = new H1("Añada las características de su compra. ");
+        TextField Nombre = new TextField("Nombre Compra");
+        DatePicker datePicker = new DatePicker("Fecha de Compra");
+        add(datePicker);
+        Button confirmar = new Button("Confirmar");
+        Button atras = new Button("Atrás");
+        HorizontalLayout horizontalLayout1 = new HorizontalLayout();
+        horizontalLayout1.add(Nombre, datePicker);
+        HorizontalLayout horizontalLayout2 = new HorizontalLayout();
+        horizontalLayout2.add(confirmar, atras);
+        this.add(h1, horizontalLayout1, horizontalLayout2);
+
+        confirmar.addClickListener(ButtonClickevent -> {
+            compras.setNombre(Nombre.getValue());
+
+            LocalDate fechaSeleccionada = datePicker.getValue();
+            String fechaCompra = String.valueOf(fechaSeleccionada);  // Convierte LocalDate a java.util.Date
+            compras.setFechaCompra(fechaCompra);
+            if(persona.getCompras() != null){
+                compras.setId(String.valueOf(persona.getCompras().size() + 1));
+            }else{
+                compras.setId(String.valueOf(0));
+            }
+
+            data.anhadirCompras(compras, persona);
+            removeAll();
+            GridsView gridsView = new GridsView();
+            try {
+                gridsView.PaginaGridsView(persona);
+            } catch (URISyntaxException e) {
+                throw new RuntimeException(e);
+            }
+            this.add(gridsView);
+        });
+
+        atras.addClickListener(ButtonClickevent -> {
+            try {
+                removeAll();
+                GridsView gridsView = new GridsView();
+                gridsView.PaginaGridsView(persona);
+                this.add(gridsView);
+            } catch (URISyntaxException e) {
+                throw new RuntimeException(e);
+            }
+        });
 
 
     }
