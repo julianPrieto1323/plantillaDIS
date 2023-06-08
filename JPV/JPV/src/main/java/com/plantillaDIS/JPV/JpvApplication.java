af0ce5a3d82c20f70e1499762925bb42d6abe5e0
julianPrieto1323 <9000169@alumnos.ufv.es>
2023-06-08 13:20:59 +0200

    Back Inicializado

---
+++ JPV/JPV/src/main/java/com/plantillaDIS/JPV/JpvApplication.java
@@ -0,0 +1,13 @@
+package com.plantillaDIS.JPV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpvApplication.class, args);
+	}
+
+}
