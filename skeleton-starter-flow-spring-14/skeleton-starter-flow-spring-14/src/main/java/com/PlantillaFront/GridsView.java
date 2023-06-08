f18feefc311a1489fefe971eb71dd1d3599655c7
julianPrieto1323 <9000169@alumnos.ufv.es>
2023-06-08 15:32:00 +0200

    Comienzo a trabajar en mostrar las compras de un usuario que inicia sesión

---
+++ skeleton-starter-flow-spring-14/skeleton-starter-flow-spring-14/src/main/java/com/PlantillaFront/GridsView.java
@@ -0,0 +1,22 @@
+package com.PlantillaFront;
+
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import org.atmosphere.interceptor.AtmosphereResourceStateRecovery;
+import org.vaadin.example.Compras;
+import org.vaadin.example.Persona;
+
+import javax.swing.plaf.basic.BasicOptionPaneUI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GridsView extends VerticalLayout {
+    DataService data = new DataService();
+    public void PaginaGridsView(Persona persona) throws URISyntaxException {
+
+    }
+}
---
+++ skeleton-starter-flow-spring-14/skeleton-starter-flow-spring-14/src/main/java/com/PlantillaFront/VistaCompras.java
@@ -0,0 +1,14 @@
+package com.PlantillaFront;
+
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import org.vaadin.example.Compras;
+import org.vaadin.example.Persona;
+
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
+public class VistaCompras extends VerticalLayout {
+
+    }
+}
