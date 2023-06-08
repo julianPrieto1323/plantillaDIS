13180e80b3b9ec530c3258c9f5ec8b29a97ca191
julianPrieto1323 <9000169@alumnos.ufv.es>
2023-06-08 14:32:27 +0200

    Clase compras Creada en el Front

--- skeleton-starter-flow-spring-14/skeleton-starter-flow-spring-14/src/main/java/com/PlantillaFront/Compras.java
+++ skeleton-starter-flow-spring-14/skeleton-starter-flow-spring-14/src/main/java/com/PlantillaFront/Compras.java
@@ -1,5 +1,57 @@
 package org.vaadin.example;
 
 public class Compras {
-    
+    private String id;
+    private String nombre;
+    private String fechaCompra;
+
+    public Compras(String id, String nombre, String fechaCompra) {
+        this.id = id;
+        this.nombre = nombre;
+        this.fechaCompra = fechaCompra;
+    }
+
+    public Compras() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFechaCompra() {
+        return fechaCompra;
+    }
+
+    public void setFechaCompra(String fechaCompra) {
+        this.fechaCompra = fechaCompra;
+    }
+
+    public String mostrarJson() {
+        return "{\n" +
+                "\"id\": " +  id + "," + "\n" +
+                "\"nombre\": " + "\"" + nombre  + "\"," + "\n" +
+                "\"fechaCompra\": " + "\"" + fechaCompra + "\"" +  ",\n" +
+                "}";
+    }
+
+    @Override
+    public String toString() {
+        return "Compras{" +
+                "id='" + id + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", fechaCompra=" + fechaCompra +
+                '}';
+    }
 }
