4c467eed4270b2d21184629b14462d0c2b318839
julianPrieto1323 <9000169@alumnos.ufv.es>
2023-06-08 15:38:05 +0200

    Ya se muestran las compras

--- skeleton-starter-flow-spring-14/skeleton-starter-flow-spring-14/src/main/java/com/PlantillaFront/GridsView.java
+++ skeleton-starter-flow-spring-14/skeleton-starter-flow-spring-14/src/main/java/com/PlantillaFront/GridsView.java
@@ -17,6 +17,43 @@ import java.util.List;
 public class GridsView extends VerticalLayout {
     DataService data = new DataService();
     public void PaginaGridsView(Persona persona) throws URISyntaxException {
+        H1 h1 = new H1("Mostrando Compras de " + persona.getNombre());
 
+        Grid<Compras> grid = new Grid<>(Compras.class, false);
+        grid.addColumn(Compras::getId).setHeader("Id");
+        grid.addColumn(Compras::getNombre).setHeader("Nombre");
+        grid.addColumn(Compras::getFechaCompra).setHeader("Fecha");
+        ArrayList<Compras> listaCompras = data.getCompras(persona);
+        if(listaCompras != null){
+            grid.setItems(listaCompras);
+        }
+
+
+        Button eliminar = new Button("Borrar Usuario");
+        eliminar.addClickListener( ButtonClickevent -> {
+            removeAll();
+            data.eliminarUsuario(persona);
+            LoginView loginView = new LoginView();
+            loginView.PaginaLoginWiew();
+            this.add(loginView);
+        });
+
+        Button anhadirCompras = new Button("Añadir Compras");
+        anhadirCompras.addClickListener( ButtonClickevent -> {
+            removeAll();
+            Anhadir_Compras anhadirCompras1 = new Anhadir_Compras();
+            anhadirCompras1.Anhadir_ComprasView(persona);
+            this.add(anhadirCompras1);
+        });
+
+        Button editarUsuario = new Button("Editar Usuario");
+        editarUsuario.addClickListener( event -> {
+            removeAll();
+            EditarUsuario editarUsuario1 = new EditarUsuario();
+            editarUsuario1.paginaEditar(persona);
+            persona.setCompras(data.mostrarCompras(persona));
+            this.add(editarUsuario1);
+        });
+        this.add(h1, grid, eliminar, anhadirCompras, editarUsuario);
     }
 }
--- skeleton-starter-flow-spring-14/skeleton-starter-flow-spring-14/src/main/java/com/PlantillaFront/VistaCompras.java
+++ skeleton-starter-flow-spring-14/skeleton-starter-flow-spring-14/src/main/java/com/PlantillaFront/VistaCompras.java
@@ -9,6 +9,14 @@ import java.net.URISyntaxException;
 import java.util.ArrayList;
 
 public class VistaCompras extends VerticalLayout {
+    DataService data = new DataService();
+    public void comprasView(Persona persona) throws URISyntaxException {
+        Grid<Compras> grid = new Grid<>(Compras.class, false);
+        grid.addColumn(Compras::getId).setHeader("Id");
+        grid.addColumn(Compras::getNombre).setHeader("Nombre");
+        grid.addColumn(Compras::getFechaCompra).setHeader("Fecha Compra");
 
+        ArrayList<Compras> compras = data.getCompras(persona);
+        grid.setItems(compras);
     }
 }
