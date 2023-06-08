dd02fb49002b8777ece9f578f73c479f24ffeb9a
julianPrieto1323 <9000169@alumnos.ufv.es>
2023-06-08 15:13:20 +0200

    Comienzo a crear la vista de registro

---
+++ skeleton-starter-flow-spring-14/skeleton-starter-flow-spring-14/src/main/java/com/PlantillaFront/RegisterView.java
@@ -0,0 +1,20 @@
+package com.PlantillaFront;
+
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.textfield.TextField;
+import org.vaadin.example.Persona;
+
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
+public class RegisterView extends VerticalLayout {
+    public void paginaRegister() {
+        
+    }
+}
